6dea4047df0e5d355a776e18e53bf0bf2375ce62
Ben Hopkins <bjhops17@gmail.com>
2021-04-12 14:03:13 -0700

    added submit functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 const searchButton = document.querySelector(".searchButton");
+const formSubmit = document.querySelector(".submit")
 const inputValue = document.querySelector("#input_text");
 let savedData;
 function specificDetails(id) {
@@ -71,14 +72,111 @@ function showSlides() {
 
 /////////////////////////////////////////////////////////////
 
-searchButton.addEventListener("click", function (event) {
+// searchButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   let hide = document.getElementsByClassName("main-card");
+//   for (let i = 0; i < hide.length; i++) {
+//     hide[i].style.display = "none";
+//   }
+
+  
+//   fetch(
+//     `https://api.themoviedb.org/3/search/movie?api_key=dc7d76692b192b772ecce4d938dfa475&language=en-US&query=%22${inputValue.value}%22&page=1&include_adult=false`,
+//     {
+//       method: "GET",
+//     }
+//   )
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.results.length === 0) {
+//       } else {
+//         for (let i = 0; i < data.results.length; i++) {
+//           let row = document.createElement("div");
+//           row.classList.add("row");
+//           row.classList.add("card");
+//           row.classList.add("main-card");
+//           row.style.backgroundColor = "#282828"
+//           let col = document.createElement("div");
+//           col.classList.add("col");
+//           col.classList.add("s12");
+//           col.style.backgroundColor = "#282828"
+          
+//           let card = document.createElement("div");
+//           card.classList.add("card");
+//           let imgCont = document.createElement("div");
+//           imgCont.classList.add("card-image");
+//           let im = document.createElement("img");
+//           im.setAttribute(
+//             "src",
+//             `https://image.tmdb.org/t/p/w500${data.results[i].backdrop_path}`
+//           );
+//           let content = document.createElement("div");
+//           content.classList.add("card-content");
+//           let p = document.createTextNode(data.results[i].overview);
+//           let btnCont = document.createElement("div");
+//           btnCont.classList.add("card-action");
+//           let a = document.createElement("button");
+//           a.setAttribute("class", "specificButton");
+//           a.href = "/specific"
+//           a.addEventListener("click", function (event) {
+            
+//             specificDetails(event.target.value);
+            
+//           });
+//           let atext = document.createTextNode(data.results[i].title);
+
+
+//           let ratingCont = document.createElement("div")
+//           ratingCont.style.display = "flex"
+//           ratingCont.style.alignItems = "center"
+          
+
+//           let ratingP = document.createElement("div");
+//           ratingP.style.display = "flex"
+//           ratingP.style.justifyContent = "space-between"
+          
+//           let icon = document.createElement("icon")
+//           icon.classList.add("material-icons")
+//           icon.textContent = "star_border"
+          
+          
+//           let textRating = document.createTextNode(
+//             data.results[i].vote_average
+//           );
+          
+//           let main = document.querySelector(".main");
+//           main.append(row);
+//           row.append(col);
+//           col.append(card);
+//           card.append(imgCont);
+//           card.append(content);
+//           card.append(btnCont);
+//           imgCont.append(im);
+//           content.append(p);
+//           // btnCont.append(a);
+//           btnCont.append(ratingP)
+//           ratingP.append(a)
+//           ratingP.append(ratingCont)
+//           ratingCont.append(icon)
+//           ratingCont.append(textRating)
+//           btnCont.append(ratingP);
+
+//           a.append(atext);
+//           a.setAttribute("value", data.results[i].id);
+//         }
+//       }
+      
+//     })
+//     .catch((err) => console.error(err));
+// });
+
+/////////submit
+formSubmit.addEventListener("submit", function (event) {
   event.preventDefault();
   let hide = document.getElementsByClassName("main-card");
   for (let i = 0; i < hide.length; i++) {
     hide[i].style.display = "none";
   }
-
- 
 
   
   fetch(
@@ -89,6 +187,7 @@ searchButton.addEventListener("click", function (event) {
   )
     .then((res) => res.json())
     .then((data) => {
+      inputValue.value = ""
       if (data.results.length === 0) {
       } else {
         for (let i = 0; i < data.results.length; i++) {
